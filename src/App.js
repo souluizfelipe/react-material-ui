ea41bad0577fc77c4ef645c230c6ba9266d86db5
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-22 10:12:46 -0300

    add material ui components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import Container from '@material-ui/core/Container';
+import { TextField, Button } from '@material-ui/core';
 
 import './App.css';
 
 function App() {
+
   return (
     <>
-            
+      <Container maxWidth="md">
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <form>
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+        </form>  
+      </Container>
     </>
   );
 }
